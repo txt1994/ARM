8ea73576ca101fe614bf37dc9c23d7d3c6efa31c
txt1994 <linguangyuan88@gmail.com>
2023-07-06 14:06:56 +0800

    小华(原名华大)Keil模版提交

---
+++ HC32_Template/User/main.c
@@ -0,0 +1,12 @@
+#include "hc32f4a0sitb.h"
+
+/**
+ * @brief  Main function of template project
+ * @param  None
+ * @retval int32_t return value, if needed
+ */
+int32_t main(void) {
+    /* Add your code here */
+    while (1) {
+    }
+}
